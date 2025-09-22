000427921d5e46238970d756177c451312797467
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2025-09-22 13:58:59 +0800

    Added conditional poss of beta

--- example.jl
+++ example.jl
@@ -1,6 +1,6 @@
 using Distributions, LinearAlgebra, Random
 using Optim
-using Plots
+using Plots, LaTeXStrings
 
 # reduced-form likelihood function (on log scale)
 function ll_rf(Γ, Ψ, W, Z)
@@ -26,7 +26,7 @@ function f_str(α, β, W, Z)
 end
 
 # simulate data
-α, β = ([0.0], 1)
+α, β = ([0.0], 1.0)
 Σ = [1.0 0.5; 0.5 1.0]
 
 Ψ = [1.0 β; 0.0 1.0] * Σ * [1.0 β; 0.0 1.0]'
@@ -58,6 +58,29 @@ plot(aa, bb, zz, st = :surface, xlabel = "α", ylabel = "β", zlabel ="Posterior
 
 # contour plot
 plot(aa, bb, zz, xlabel = "α", ylabel = "β", zlabel ="Posterior Possibility (log)")
+
+
+## conditional possibility of β ##
+function f_β_given_α(β, lower, upper, W, Z) 
+    f(a) = -f_str([a], β, W, Z)
+    res = optimize(f, lower, upper, Brent())
+    α_opt = Optim.minimizer(res)
+    return round(f_str([α_opt], β, W, Z), digits = 10)
+end
+
+# plot with simulated data
+bb = -1.0:0.01:3.0
+yy_unconstrained = map(b -> exp(f_β_given_α(b, -1e2, 1e2, W, Z)), bb)
+yy_1 = map(b -> exp(f_β_given_α(b, -1, 1, W, Z)), bb)
+yy_14 = map(b -> exp(f_β_given_α(b, -1/4, 1/4, W, Z)), bb)
+yy_0 = map(b -> exp(f_str([0.0], b, W, Z)), bb)
+
+plot(bb, yy_unconstrained, xlabel = L"\beta", label = L"| \alpha | \leq 100")
+plot!(bb, yy_1, xlabel = L"\beta", label = L"| \alpha |  \leq 1")
+plot!(bb, yy_14, xlabel = L"\beta", label = L"| \alpha |  \leq 1/4")
+plot!(bb, yy_0, xlabel = L"\beta", label = L"\alpha = 0")
+
+
 
 
 
