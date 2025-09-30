2cd4443aa81653ff8413f6f30f4f6131db4eab9a
Gregor Steiner <51028286+gregorsteiner@users.noreply.github.com>
2025-09-30 11:12:11 +0800

    Added raw possibilities to the plot

--- AJR_example.jl
+++ AJR_example.jl
@@ -14,31 +14,26 @@ W = [ones(length(y_raw)) Matrix(d[:, ["Latitude", "Africa", "Asia", "Namer", "Sa
 M_W = I - W * inv(W'W) * W'
 y, x, z = map(vec -> M_W * vec, (y_raw, x_raw, z_raw))
 
-# plot raw possibilistic posterior
-posterior(β, bound) = exp(f_β_given_α(β, -bound, bound, [y x], z))
-xx = -20:0.1:20
-plot(
-    xx, posterior.(xx, 0.0),
-    xlabel = L"\beta", ylabel = "Posterior Possibility",
-    label = L"\alpha = 0"
-)
-plot!(xx, posterior.(xx, 1/10), label = L"\alpha \in [-0.1, 0.1]")
+# raw possibilistic posterior
+posterior(β, lower, upper) = exp(f_β_given_α(β, [lower], [upper], [y x], z))
 
-
-
-# plot possibilistic contour
+# possibilistic contour
 pi_w(β, lower, upper) = possibilistic_contour(β, [lower], [upper], [y x], z)
 
-xx = -4:0.005:8
+
+# Create plot
+xx = -3:0.005:6
 p = plot(
     xx, pi_w.(xx, 0.0, 0.0),
     linewidth = 1.5,
-    xlabel = L"\beta", ylabel = "Possibilistic Contour",
+    xlabel = L"\beta", ylabel = "",
     label = L"\alpha = 0"
 )
-plot!(xx, pi_w.(xx, -1/4, 1/4), linewidth = 1.5, label = L"\alpha \in [-0.1, 0.1]")
-hline!([0.1], linestyle = :dash, label = "")
-plot!(res)
+plot!(xx, pi_w.(xx, -1/10, 1/10), linewidth = 1.5, label = L"\alpha \in [-0.1, 0.1]")
+plot!(xx, posterior.(xx, 0.0, 0.0), linestyle = :dash, linewidth = 1.5, color = 1, label = "")
+plot!(xx, posterior.(xx, -1/10, 1/10), linestyle = :dash, linewidth = 1.5, color = 2, label = "")
+
+hline!([0.1], linestyle = :dash, label = "", colour = :grey)
 
 savefig(p, "AJR_Possibility_Contour.pdf")
 
